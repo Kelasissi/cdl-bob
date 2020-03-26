9aaf5be4faa9e6d002a7b3d3fbf294f8300effae
titi <Thierry57410@gmail.com>
2020-03-26 18:47:43 +0100

    Correction bug léger + modif fin

---
+++ BOB_2/Assets/Script/ChangeScene_Final.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene_Final : MonoBehaviour
+{
+
+    private int nextSceneToLoad;
+    public bool activated = false;
+
+    private void Start()
+    {
+        nextSceneToLoad = 1;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player") && activated == true)
+        {
+            SceneManager.LoadScene(nextSceneToLoad);
+            // Monter la scale du perso
+            // Baisser sa vitesse et la force de son saut.
+        }
+
+    }
+}
